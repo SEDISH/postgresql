a0ec97ec8cdf06a4a112a76514b475be685e93db
Marcin Kruszyński <mkruszynski@soldevelo.com>
2017-05-24 15:42:02 +0200

    #84: Parametrization of copy script

--- copy.sh
+++ copy.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-psql -d dhis2 -U postgres -c "copy attribute from '/tmp/attributes.csv' delimiter ',' csv" > /dev/null
-psql -d dhis2 -U postgres -c "copy attributevalue from '/tmp/fields_values.csv' delimiter ',' csv" > /dev/null
-psql -d dhis2 -U postgres -c "copy organisationunit from '/tmp/facilities.csv' delimiter ',' csv" > /dev/null
-psql -d dhis2 -U postgres -c "copy organisationunitattributevalues from '/tmp/organisationunitattributevalues.csv' delimiter ',' csv" > /dev/null
-
-#HWR
-psql -d dhis2 -U postgres -c "copy userinfo from '/tmp/userInfo.csv' delimiter ',' csv" > /dev/null
-psql -d dhis2 -U postgres -c "copy userattributevalues from '/tmp/userAttributeValues.csv' delimiter ',' csv" > /dev/null
-psql -d dhis2 -U postgres -c "copy users from '/tmp/users.csv' delimiter ',' csv" > /dev/null
-psql -d dhis2 -U postgres -c "copy userrole from '/tmp/userRole.csv' delimiter ',' csv" > /dev/null
-psql -d dhis2 -U postgres -c "copy userroleauthorities from '/tmp/userRoleAuthorities.csv' delimiter ',' csv" > /dev/null
-psql -d dhis2 -U postgres -c "copy userrolemembers from '/tmp/userRoleMembers.csv' delimiter ',' csv" > /dev/null
-psql -d dhis2 -U postgres -c "copy usermembership from '/tmp/userMembership.csv' delimiter ',' csv" > /dev/null
-psql -d dhis2 -U postgres -c "copy usersetting from '/tmp/userSetting.csv' delimiter ',' csv" > /dev/null
+if [[ $1 =~ ^[Ff][Rr] ]]; then
+	#FR
+	psql -d dhis2 -U postgres -c "copy attribute from '/tmp/attributes.csv' delimiter ',' csv" > /dev/null
+	psql -d dhis2 -U postgres -c "copy attributevalue from '/tmp/fields_values.csv' delimiter ',' csv" > /dev/null
+	psql -d dhis2 -U postgres -c "copy organisationunit from '/tmp/facilities.csv' delimiter ',' csv" > /dev/null
+	psql -d dhis2 -U postgres -c "copy organisationunitattributevalues from '/tmp/organisationunitattributevalues.csv' delimiter ',' csv" > /dev/null
+elif [[ $1 =~ ^[Hh][Ww][Rr] ]]; then
+	#HWR
+	psql -d dhis2 -U postgres -c "copy userinfo from '/tmp/userInfo.csv' delimiter ',' csv" > /dev/null
+	psql -d dhis2 -U postgres -c "copy userattributevalues from '/tmp/userAttributeValues.csv' delimiter ',' csv" > /dev/null
+	psql -d dhis2 -U postgres -c "copy users from '/tmp/users.csv' delimiter ',' csv" > /dev/null
+	psql -d dhis2 -U postgres -c "copy userrole from '/tmp/userRole.csv' delimiter ',' csv" > /dev/null
+	psql -d dhis2 -U postgres -c "copy userroleauthorities from '/tmp/userRoleAuthorities.csv' delimiter ',' csv" > /dev/null
+	psql -d dhis2 -U postgres -c "copy userrolemembers from '/tmp/userRoleMembers.csv' delimiter ',' csv" > /dev/null
+	psql -d dhis2 -U postgres -c "copy usermembership from '/tmp/userMembership.csv' delimiter ',' csv" > /dev/null
+	psql -d dhis2 -U postgres -c "copy usersetting from '/tmp/userSetting.csv' delimiter ',' csv" > /dev/null
+fi
